01ad5869c3fe6192ed78742847273ca255555e60
gaaaron <garancsya@gmail.com>
2024-11-04 19:25:13 +0100

    Convert the static home page to dynamic

---
+++ UserRepositoryTest/Services/IUserAccountService.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRepositoryTest.Services;
+
+public interface IUserAccountService
+{
+    public GetUsersResult GetUsers(UserFilterModel filter);
+}
+
+public record UserFilterModel(int Page, int Take);
+
+public record GetUsersResult(IEnumerable<UserModel> Users, int CurrentPage, int MaxPage);
---
+++ UserRepositoryTest/Services/UserAccountService.cs
@@ -0,0 +1,14 @@
+﻿
+namespace UserRepositoryTest.Services;
+
+public class UserAccountService(IUserRepository userRepository) : IUserAccountService
+{
+    public GetUsersResult GetUsers(UserFilterModel filter)
+    {
+        var users = userRepository.Users.OrderBy(x => x.Name).ToList();
+        var usersResult = users.Skip((filter.Page - 1) * filter.Take).Take(filter.Take);
+        var maxPage = users.Count() / filter.Take;
+
+        return new GetUsersResult(usersResult, filter.Page, maxPage);
+    }
+}
